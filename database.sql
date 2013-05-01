ca6c1cb321e8e6cf4f45aa45b9c0c3b0ae23b48c
Jacek <jacek@kuzemczak.co.uk>
2013-05-01 21:32:19 +0100

    Added SQL for sessions table.

--- database.sql
+++ database.sql
@@ -39,11 +39,27 @@ CREATE TABLE IF NOT EXISTS `stats_players` (
   `player_total_chat` int(8) NOT NULL default '0',
   PRIMARY KEY  (`player_id`),
   UNIQUE KEY `unique` (`player_name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `stats_player_kills` (
   `player_id` int(8) NOT NULL,
   `killed_player_id` int(8) NOT NULL,
   `total` int(12) NOT NULL,
   UNIQUE KEY `unique` (`player_id`,`killed_player_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `stats_sessions` (
+  `session_id` int(10) NOT NULL auto_increment,
+  `player_id` int(8) unsigned NOT NULL,
+  `session_time` int(10) NOT NULL,
+  `session_status` smallint(1) NOT NULL DEFAULT '1',
+  `session_blocks_placed` int(10) unsigned NOT NULL DEFAULT '0',
+  `session_blocks_broken` int(10) unsigned NOT NULL DEFAULT '0',
+  `session_time_spend` int(8) NOT NULL DEFAULT '0',
+  `session_time_active` int(8) NOT NULL DEFAULT '0',
+  `session_distance_travelled` int(8) unsigned NOT NULL DEFAULT '0',
+  `session_mob_deaths` int(11) NOT NULL DEFAULT '0',
+  `session_player_deaths` int(11) NOT NULL DEFAULT '0',
+  `session_suicides` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`session_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
